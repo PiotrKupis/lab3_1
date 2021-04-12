0001eb1cd62ed080aee073bb472d60df82c46d80
Kupis.Piotr <222488@edu.p.lodz.pl>
2021-04-12 21:17:07 +0200

    Added ProductDataBuilder

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,20 +1,36 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith((MockitoExtension.class))
 class BookKeeperTest {
 
-    @BeforeEach
-    void setUp() throws Exception {
+    private static final String DUMMY_CLIENT_NAME = "Nowak";
+    private static final ClientData DUMMY_CLIENT = new ClientData(Id.generate(), DUMMY_CLIENT_NAME);
 
+    @Mock
+    private InvoiceFactory invoiceFactory;
+    @Mock
+    private TaxPolicy taxPolicy;
+    private BookKeeper bookKeeper;
+    private InvoiceRequest invoiceRequest;
+
+    @BeforeEach
+    void setUp() {
+        bookKeeper = new BookKeeper(invoiceFactory);
+        invoiceRequest = new InvoiceRequest(DUMMY_CLIENT);
     }
 
     @Test
-    void test() {
-        fail("not implemented");
+    void givenOneItemWhenInvoiceIssuanceThenReturnsOneItemInvoice() {
     }
 
 }
